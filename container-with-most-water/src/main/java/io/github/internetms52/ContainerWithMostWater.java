deacca6e44bfa512ab05519f4741a68913a76d24
internetms52 <internetms52@gmail.com>
2025-02-07 16:01:15 +0800

    add container-with-most-water

---
+++ container-with-most-water/src/main/java/io/github/internetms52/ContainerWithMostWater.java
@@ -0,0 +1,43 @@
+package io.github.internetms52;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int result = 0;
+        int maxW = height.length-1;
+        for(int i =0;i<height.length-1;i++){
+            if(result!=0){
+                if(height[i]==0){
+                    continue;
+                }
+                if(height[i]!=0 && result/height[i]>maxW){
+                    continue;
+                }
+            }
+            for(int j =1;j<height.length;j++){
+                if(result>0 && height[j]==0){
+                    continue;
+                }
+                if(i+j<height.length){
+                    int from = height[i];
+                    int to = height[i+j];
+                    int tmpResult = min(from,to) * j;
+                    if(tmpResult>result){
+                        result = tmpResult;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public int min(int a,int b){
+        if(a<b){
+            return a;
+        }else{
+            return b;
+        }
+    }
+}
