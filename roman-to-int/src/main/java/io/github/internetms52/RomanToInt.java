7b7f8ef0e1e300c36b7cf6923a696fc8f22517b7
internetms52 <internetms52@gmail.com>
2025-02-10 12:29:50 +0800

    update

--- roman-to-int/src/main/java/io/github/internetms52/RomanToInt.java
+++ roman-to-int/src/main/java/io/github/internetms52/RomanToInt.java
@@ -2,54 +2,44 @@ package io.github.internetms52;
 
 public class RomanToInt {
     int[] romanNums = new int[]{1, 5, 10, 50, 100, 500, 1000};
-    String[] romanWords = new String[]{"I", "V", "X", "L", "C", "D", "M"};
+    char[] romanWords = new char[]{'I','V','X','L','C','D','M'};
 
-    public String intToRoman(int num) {
-        return romanConvertRecursively(num);
-    }
-
-    int getByDecimalPlaces(int power, int num) {
-        return num / Double.valueOf(Math.pow(10, power)).intValue() % 10;
-    }
-
-    int revertTenBaseNum(int power, int num) {
-        return num * Double.valueOf(Math.pow(10, power)).intValue();
-    }
-
-    String romanConvertRecursively(int num) {
-        StringBuilder result = new StringBuilder();
-        int numLength = String.valueOf(num).length();
-        for (int i = 0; i < numLength; i++) {
-            int d = getByDecimalPlaces(i, num);
-            if (d == 0) {
-                continue;
-            }
-            for (int j = 0; j < romanNums.length; j++) {
-                int tenBaseNum = revertTenBaseNum(i, d);
-                if (romanNums[j] == tenBaseNum) {
-                    result.insert(0, romanWords[j]);
-                    break;
+    public int romanToInt(String s) {
+        char[] sCharArray = s.toCharArray();
+        int prevCIdx=-1;
+        int result=0;
+        for(int i=sCharArray.length-1;i>=0;i--){
+            if(prevCIdx<0){
+                if(i==0){
+                    result+= romanNums[mapIdx(sCharArray[i])];
+                }else{
+                    prevCIdx = mapIdx(sCharArray[i]);
                 }
-                if (romanNums[j] > tenBaseNum) {
-                    if (d == 4 || d == 9) {
-                        int remains = romanNums[j] - tenBaseNum;
-                        if (remains > 0) {
-                            result.insert(0, romanConvertRecursively(remains) + romanWords[j]);
-                            break;
-                        }
+            }else{
+                int cIdx = mapIdx(sCharArray[i]);
+                if(prevCIdx>cIdx){
+                    //4 or 9
+                    result+=romanNums[prevCIdx] - romanNums[cIdx];
+                    prevCIdx=-1;
+                }else{
+                    if(i==0){
+                        result+=romanNums[prevCIdx] + romanNums[cIdx];
+                    }else{
+                        result+=romanNums[prevCIdx];
+                        prevCIdx=cIdx;
                     }
-                    if (j - 1 >= 0) {
-                        int remains = tenBaseNum - romanNums[j - 1];
-                        result.insert(0, romanWords[j - 1] + romanConvertRecursively(remains));
-                        break;
-                    }
-                } else if (j + 1 == romanNums.length) {
-                    int remains = tenBaseNum - romanNums[j];
-                    result.insert(0, romanWords[j] + romanConvertRecursively(remains));
-                    break;
                 }
             }
         }
-        return result.toString();
+        return result;
+    }
+
+    int mapIdx(char c){
+        for(int i=0;i<romanWords.length;i++){
+            if(c == romanWords[i]){
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- roman-to-int/src/test/java/io/github/internetms52/RomanToIntTest.java
+++ roman-to-int/src/test/java/io/github/internetms52/RomanToIntTest.java
@@ -4,12 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RomanToIntTest {
-    private static final RomanToInt INT_TO_ROMAN = new RomanToInt();
+    private static final RomanToInt ROMAN_TO_INT = new RomanToInt();
 
     @Test
     void case1() {
-        Assertions.assertEquals(INT_TO_ROMAN.intToRoman(3749),"MMMDCCXLIX");
+        Assertions.assertEquals(ROMAN_TO_INT.romanToInt("III"),3);
     }
 
+    @Test
+    void case2() {
+        Assertions.assertEquals(ROMAN_TO_INT.romanToInt("LVIII"),58);
+    }
 
+    @Test
+    void case3() {
+        Assertions.assertEquals(ROMAN_TO_INT.romanToInt("MCMXCIV"),1994);
+    }
+
+    @Test
+    void case4() {
+        Assertions.assertEquals(ROMAN_TO_INT.romanToInt("MCDLXXVI"),1476);
+    }
 }
