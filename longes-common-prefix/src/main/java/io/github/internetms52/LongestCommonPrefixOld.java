8094c88c9525ef60095147ac1655474656199369
internetms52 <internetms52@gmail.com>
2025-02-11 13:48:29 +0800

    update

---
+++ longes-common-prefix/src/main/java/io/github/internetms52/LongestCommonPrefixOld.java
@@ -0,0 +1,51 @@
+package io.github.internetms52;
+
+@Deprecated
+public class LongestCommonPrefixOld {
+    private char[][] charCache;
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==1){
+            return strs[0];
+        }
+
+        String result = "";
+        //i horizontal index
+        //j vertical index
+        for(int i=0;i<getCharArray(strs,0).length;i++){
+            char mapChar='\u0000';
+            boolean isBreak = false;
+            for(int j=0;j<strs.length;j++){
+                char[] chars = getCharArray(strs,j);
+                if(i>=chars.length){
+                    break;
+                }
+                if(mapChar=='\u0000'){
+                    mapChar = chars[i];
+                    continue;
+                }
+                if(chars[i]!=mapChar){
+                    isBreak=true;
+                    break;
+                }
+                if(j+1==strs.length && mapChar!='\u0000'){
+                    result+=mapChar;
+                }
+            }
+            if(isBreak){
+                break;
+            }
+        }
+        return result;
+    }
+
+    public char[] getCharArray(String[] strs,int idx){
+        if(charCache==null){
+            charCache = new char[strs.length][];
+        }
+        if(charCache[idx]==null){
+            charCache[idx] = strs[idx].toCharArray();
+        }
+        return charCache[idx];
+    }
+
+}
--- longes-common-prefix/src/test/java/io/github/internetms52/LongestCommonPrefixTest.java
+++ longes-common-prefix/src/test/java/io/github/internetms52/LongestCommonPrefixTest.java
@@ -1,12 +1,31 @@
 package io.github.internetms52;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LongestCommonPrefixTest {
     LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+
     @Test
-    public void test1(){
-        String result = longestCommonPrefix.longestCommonPrefix(new String[]{"flower","flower","flower","flower"});
-        System.out.println(result);
+    public void test1() {
+        String result = longestCommonPrefix.longestCommonPrefix(new String[]{"flower", "flower", "flower", "flower"});
+        Assertions.assertEquals("flower", result);
+    }
+
+    @Test
+    public void test2() {
+        String result = longestCommonPrefix.longestCommonPrefix(new String[]{"flower","flow","flight"});
+        Assertions.assertEquals("fl", result);
+    }
+
+    @Test
+    public void test3() {
+        String result = longestCommonPrefix.longestCommonPrefix(new String[]{"a"});
+        Assertions.assertEquals("a", result);
+    }
+    @Test
+    public void test4() {
+        String result = longestCommonPrefix.longestCommonPrefix(new String[]{"cir","car"});
+        Assertions.assertEquals("c", result);
     }
 }
