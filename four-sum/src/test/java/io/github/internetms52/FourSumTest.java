f2a04f1f3ebb6c33b98eddf2cd565456baefbd6f
internetms52 <internetms52@gmail.com>
2025-02-19 08:27:56 +0800

    使用外部library

--- four-sum/src/test/java/io/github/internetms52/FourSumTest.java
+++ four-sum/src/test/java/io/github/internetms52/FourSumTest.java
@@ -1,68 +1,21 @@
 package io.github.internetms52;
 
+import hobby.internetms52.leetcode.validation.ListOfIntegerLists;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FourSumTest {
     FourSum fourSum = new FourSum();
-
-    public boolean check(List<Integer> aList, List<Integer> bList) {
-        if (aList.size() != bList.size()) {
-            return false;
-        }
-        HashSet<Integer> aSet = new HashSet<>(aList);
-        HashSet<Integer> bSet = new HashSet<>(bList);
-        return aSet.equals(bSet);
-    }
-
-    public boolean checkResult(List<List<Integer>> resultList, List<List<Integer>> targetList) {
-        if (resultList.size() != targetList.size()) {
-            return false;
-        } else {
-            List<List<Integer>> sortedResultList = resultList.stream().map(list -> {
-                return list.stream().sorted().collect(Collectors.toList());
-            }).sorted((list1, list2) -> {
-                if (list1.size() != list2.size()) {
-                    return Integer.compare(list1.size(), list2.size());
-                }
-                for (int i = 0; i < list1.size(); i++) {
-                    int compare = list1.get(i).compareTo(list2.get(i));
-                    if (compare != 0) return compare;
-                }
-                return 0;
-            }).toList();
-            List<List<Integer>> sortedTargetList = targetList.stream().map(list -> {
-                return list.stream().sorted().collect(Collectors.toList());
-            }).sorted((list1, list2) -> {
-                if (list1.size() != list2.size()) {
-                    return Integer.compare(list1.size(), list2.size());
-                }
-                for (int i = 0; i < list1.size(); i++) {
-                    int compare = list1.get(i).compareTo(list2.get(i));
-                    if (compare != 0) return compare;
-                }
-                return 0;
-            }).toList();
-            for (int i = 0; i < resultList.size(); i++) {
-                if (!check(sortedTargetList.get(i), sortedResultList.get(i))) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 
     @Test
     public void test1() {
         List<List<Integer>> result = fourSum.fourSum(new int[]{2, 2, 2, 2, 2}, 8);
         List<List<Integer>> answerList = new ArrayList<>();
         answerList.add(List.of(2, 2, 2, 2));
-        Assertions.assertTrue(checkResult(result, answerList));
+        Assertions.assertTrue(ListOfIntegerLists.isMutualContainment(result, answerList));
     }
 
     @Test
@@ -77,7 +30,7 @@ public class FourSumTest {
         List<List<Integer>> answerList = new ArrayList<>();
         answerList.add(List.of(-2, 1, -1, 2));
         answerList.add(List.of(-1, -1, 1, 1));
-        Assertions.assertTrue(checkResult(result, answerList));
+        Assertions.assertTrue(ListOfIntegerLists.isMutualContainment(result, answerList));
     }
 
     @Test
