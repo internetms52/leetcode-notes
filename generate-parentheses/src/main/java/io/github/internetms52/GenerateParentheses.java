6956b28257c797182216532d50f5f1dd504e994b
internetms52 <internetms52@gmail.com>
2025-02-25 12:38:00 +0800

    add n4

---
+++ generate-parentheses/src/main/java/io/github/internetms52/GenerateParentheses.java
@@ -0,0 +1,44 @@
+package io.github.internetms52;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class GenerateParentheses {
+
+    public List<String> generateParenthesis(int n) {
+        return new ArrayList<>(generateParenthesisRecursively(n));
+    }
+
+    public Set<String> generateParenthesisRecursively(int n){
+        Set<String> resultSet = new HashSet();
+        if(n==1){
+            resultSet = Set.of("()");
+        }else if(n-1>0) {
+            Set<String> reduceSet = generateParenthesisRecursively(n-1);
+            for(String str:reduceSet){
+                resultSet.add("()"+str);
+                resultSet.add(str+"()");
+                resultSet.add("("+str+")");
+            }
+            if(n>2){
+                Set<String> reduceGroupSet = new HashSet();
+                for(int i=2;i<n;i++){
+                    Set<String> iSet = generateParenthesisRecursively(n-i);
+                    for(int j=2;j<n;j++){
+                        Set<String> jSet = generateParenthesisRecursively(n-j);
+                        if(i+j==n){
+                            for(String iStr:iSet){
+                                for(String jStr:jSet){
+                                    resultSet.add(iStr+jStr);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return resultSet;
+    }
+}
---
+++ generate-parentheses/src/test/java/io/github/internetms52/GenerateParenthesesN4Test.java
@@ -0,0 +1,11 @@
+package io.github.internetms52;
+
+import org.junit.jupiter.api.Test;
+
+public class GenerateParenthesesN4Test {
+    GenerateParenthesesN4 generateParenthesesN4 = new GenerateParenthesesN4();
+    @Test
+    public void case2() {
+        System.out.println(generateParenthesesN4.generateParenthesis(5));
+    }
+}
